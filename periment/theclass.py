281363095dc7e003126f4581ec050e1c8a8a7c15
KevinAp-5 <keven.clash3@gmail.com>
2020-02-19 15:34:38 -0300

    adds

--- periment/theclass.py
+++ periment/theclass.py
@@ -2,20 +2,15 @@ import json
 
 
 class Periment:
-    def __init__(self, toshow='', rang=119):
+    def __init__(self, toshow='', rang=118, category=False):
         # Json path
         self.file = '/home/kevin/Periment/periment/PeriodicTableJSON.json'
-
-        if toshow != list:
-            self.toshow = []
-            self.toshow.append(toshow)
-        else:
-            self.toshow = toshow
+        self.toshow = toshow
         self.rang = rang
 
     def show(self):
-        if self.toshow == ['']:
-            self.toshow = ['name', 'symbol', 'number', 'summary']
+        if self.toshow == [''] or self.toshow == '':
+            self.toshow = ['name', 'symbol', 'number', 'category', 'summary']
 
         with open(self.file) as jsonnn:
             data = json.load(jsonnn)
@@ -23,7 +18,12 @@ class Periment:
         # Loop to show the elements
         for counter in range(self.rang):
             base = data['elements'][counter]
-            for c in range(len(self.toshow)):
-                print(base[self.toshow[c]])
-            print('')
             print('-' * 30)
+            # checks if the key exist
+            for c in self.toshow:
+                if c not in base:
+                    self.toshow.remove(c)
+
+            for c in range(len(self.toshow)):
+                print(f'{self.toshow[c]}: {base[self.toshow[c]]}')
+            print('')
