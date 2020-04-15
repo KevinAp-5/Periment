f9c1347a7f759a7c9fc10f178c3a5ce7f1b129c9
KevinAp-5 <keven.clash3@gmail.com>
2020-04-15 17:40:57 -0300

    Less lines and fix index error in main.py

--- periment/main.py
+++ periment/main.py
@@ -5,15 +5,19 @@ command = argv
 command.pop(0)
 command = fix_type(command)
 
-peri = Periment()  # Range 'til stop
-if command[0] == 'show':
-	command.pop(0)
-	peri.show(*command)
-elif command[0] == 'guess':
-	command.pop(0)
-	peri.guess(*command)
-elif command[0] == 'returning':
-	command.pop(0)
-	peri.returning(*command)
-else:
+try:
+	peri = Periment()  # Range 'til stop
+	if command[0] == 'show':
+		command.pop(0)
+		peri.show(*command)
+	elif command[0] == 'guess':
+		command.pop(0)
+		peri.guess(*command)
+	elif command[0] == 'returning':
+		command.pop(0)
+		peri.returning(*command)
+
+except Exception:
+	peri = Periment(rang=10)
 	peri.show(animation=True)
+	peri.guess()
--- periment/theclass.py
+++ periment/theclass.py
@@ -2,6 +2,7 @@ from random import shuffle
 from time import sleep
 import json
 from sys import argv
+
 def anima():
     string = '-' * 30
     for a in string:
@@ -23,11 +24,10 @@ class Periment:
         if self.b == [''] or self.b == '':
             self.b = ['name', 'symbol', 'number', 'category', 'summary']
 
+        with open(self.file) as jsonnn:
+            self.data = json.load(jsonnn)
 
     def show(self, animation=False, random=False, random_range=119):
-        with open(self.file) as jsonnn:
-            data = json.load(jsonnn)
-
         if random is True:
             random_number = list(range(0, random_range))
             shuffle(random_number)
@@ -36,14 +36,11 @@ class Periment:
         for counter in range(self.rang):
             # Random mode
             if random is True:
-                base = data['elements'][random_number[counter]]
-            else:
-                base = data['elements'][counter]
+                base = self.data['elements'][random_number[counter]]
+            else: base = self.data['elements'][counter]
 
-            if animation is True:
-                anima()
-            else:
-                print('-' * 30)
+            if animation is True: anima()
+            else: print('-' * 30)
 
             # check if the key exist in json
             for show_elements in self.b:
@@ -59,22 +56,18 @@ class Periment:
             random_number = list(range(0, random_range))
             shuffle(random_number)
 
-        with open(self.file) as jsonnn:
-            data = json.load(jsonnn)
-
         show = input('What to ask?: ').strip()
 
         for counter in range(self.rang):
             # Randomize the elements
             if random is True:
-                base = data['elements'][random_number[counter]]
-            else:
-                base = data['elements'][counter]
+                base = self.data['elements'][random_number[counter]]
+            else: base = self.data['elements'][counter]
 
             # check if the key exist in json
-            for hm in self.a:
-                if hm not in base:
-                    self.a.remove(hm)
+            for keys in self.a:
+                if keys not in base:
+                    self.a.remove(keys)
 
             if show not in base:
                 print(f"Error! {show} doesn't exist")
@@ -91,8 +84,7 @@ class Periment:
             for loop in range(len(self.a)):
                 it = base[self.a[loop]]
                 print('-' * 30)
-                print(self.a[loop], end='')
-                answer = input(': ').strip()
+                answer = input(f'{self.a[loop]}: ').strip()
 
                 if answer.isdigit() and type(it) == int:
                     answer = int(answer)
@@ -106,19 +98,14 @@ class Periment:
             print('')
 
     def returning(self, called, name, animation=True):
-        if type(name) == str:
-            name = name.capitalize()
-
-        with open(self.file) as jsonnn:
-            data = json.load(jsonnn)
+        if type(name) == str: name = name.capitalize()
 
         for counter in range(self.rang):
-            base = data['elements'][counter]
+            base = self.data['elements'][counter]
+
             if not base[called] != name:
-                if animation is True:
-                    anima()
-                else:
-                    print('-' * 30)
+                if animation is True: anima()
+                else: print('-' * 30)
 
                 for loop in range(len(self.b)):
                     print(f'{self.b[loop]}: {base[self.b[loop]]}')
