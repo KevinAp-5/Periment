f4b8c3a0564b349c6a25a3ebdd43d5ff9abab63e
KevinAp-5 <keven.clash3@gmail.com>
2020-11-16 23:38:23 -0300

    fix_type fixed!

--- fix_type.py
+++ fix_type.py
@@ -1,15 +1,12 @@
 def fix_type(argv):  # Fix TypeError caused by argv, agrv returns str
+    """ Get a list and fix the type errors"""
     for x in range(len(argv)):
-        """ Get a list and fix the type errors"""
-        if type(argv[x]) == str:
-            try:
-                argv[x] = bool(argv[x])
-            except Exception:
-                raise
-
-        try:
-            if argv[x].isdigit() is True:
+        if isinstance(argv[x], str):
+            if argv[x].isnumeric() is True:
+                argv[x] = int(argv[x])
+            elif '.' in argv[x]:
                 argv[x] = float(argv[x])
-        except Exception:
-            raise
-        return argv
+            elif argv[x] == 'True' or argv[x] == 'False':
+                argv[x] = bool(argv[x])
+
+    return argv
