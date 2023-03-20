c2af5f10ffcac142b414fd31c3ca2996bd09dcbc
KevinAp-5 <keven.clash3@gmail.com>
2023-03-19 23:46:27 -0300

    added a random search if there are no argv inputs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from periment import Periment
 from fix_type import fix_type
+from random import randint
 from sys import argv
 
 command = fix_type(argv[1:])
@@ -7,9 +8,14 @@ command = fix_type(argv[1:])
 periment = Periment()
 # periment.set_filter('name', 'number', 'symbol', 'summary')  # Custom filter
 
-if command[0] == 'show':
+try:
+    if command[0] == 'show':
+        periment.show(*command[1:])
+    elif command[0] == 'returning':
+        periment.returning(*command[1:])
+    else:
+        print('Invalid command.\ncommands: returning, show')
+except IndexError:
+    command.append('show')
+    command.append(randint(1, 119))
     periment.show(*command[1:])
-elif command[0] == 'returning':
-    periment.returning(*command[1:])
-else:
-    print('Invalid command.\ncommands: returning, show')
