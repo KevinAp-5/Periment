7ed66c56175540f6a3ee5e4f0c7279ab9c6a75bd
KevinAp-5 <keven.clash3@gmail.com>
2023-03-20 23:14:30 -0300

    Remove the fix_type file and added his function to the main file

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from periment import Periment
-from fix_type import fix_type
 from random import randint
 from sys import argv
+
+def fix_type(argv):  # Fix TypeError caused by argv, agrv returns str
+    """ Get a list and fix the type errors"""
+    for x in range(len(argv)):
+        if isinstance(argv[x], str):
+            if argv[x].isnumeric() is True:
+                argv[x] = int(argv[x])
+            elif '.' in argv[x]:
+                argv[x] = float(argv[x])
+            elif argv[x] == 'True' or argv[x] == 'False':
+                argv[x] = bool(argv[x])
+
+    return argv
 
 command = fix_type(argv[1:])
 
